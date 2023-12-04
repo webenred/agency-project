4e8c925c91eb86a26c0550a77aba8558a171d24f
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-04 18:20:47 +0100

    role features & middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasUuids;
 
     /**
      * The attributes that are mass assignable.
@@ -18,10 +20,43 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'sex',
+        'dob',
+        'phone',
         'email',
         'password',
+        'role'
     ];
+
+
+    public $incrementing = false;
+    protected $primaryKey = 'uuid';
+
+    public function hasRole($role)
+    {
+        switch ($role) {
+            case 'admin':
+                return $this->isAdmin();
+                break;
+            case 'partner':
+                return $this->isPartner();
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    public function is_admin() {
+        return $this->role === 'admin';
+    }
+
+    public function isClient() {
+        return $this->role === 'client';
+    }
+
 
     /**
      * The attributes that should be hidden for serialization.
