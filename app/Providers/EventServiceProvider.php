42c5feef8886750d3a2e50b8d09e217ef56d6dd7
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-06 23:11:00 +0100

    change the registration event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Events\RegistrationEvent;
+use App\Listeners\SendEmailVerificationNotif;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,9 +17,15 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        // Registered::class => [
+            // SendEmailVerificationNotification::class,
+        // ],
+
+        RegistrationEvent::class => [
+            SendEmailVerificationNotif::class,
         ],
+
+
     ];
 
     /**
