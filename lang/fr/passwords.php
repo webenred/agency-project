ddbe2ff19179e311d025d368c4482e132dba3cc9
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-10 18:57:20 +0100

    lang files

---
+++ lang/fr/passwords.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Messages de réinitialisation de mot de passe
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes de langue suivantes sont les lignes par défaut qui correspondent aux raisons
+    | données par le gestionnaire de mots de passe pour une tentative de mise à jour de mot de passe
+    | qui a échoué, comme pour un jeton invalide ou un nouveau mot de passe invalide.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Nous avons envoyé par e-mail le lien de réinitialisation de votre mot de passe.',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => 'Ce jeton de réinitialisation de mot de passe est invalide.',
+    'user' => "Nous ne pouvons pas trouver d'utilisateur avec cette adresse e-mail.",
+
+];
+
