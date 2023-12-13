74ffacfe4a4f43914bf855935e3f8a320e9b9284
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-13 03:52:09 +0100

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './node_modules/flowbite/**/*.js'
+        "./vendor/wire-elements/modal/resources/views/*.blade.php",
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./node_modules/flowbite/**/*.js",
     ],
+    darkMode: true,
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
-    plugins: [
-        forms,
-        require('flowbite/plugin')
-    ],
+    plugins: [forms, require("flowbite/plugin")],
 };
