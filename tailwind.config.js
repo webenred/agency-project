2fc7b69dab6cc558a12ece8631b1e85ad6ea3483
AbdenourTadjer33 <tad.abdenour33@gmail.com>
2023-12-14 15:07:57 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ import forms from "@tailwindcss/forms";
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./vendor/wire-elements/modal/resources/views/*.blade.php",
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
